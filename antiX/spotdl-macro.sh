51a1587ccff12afa9494c28d8e8c5f563447dac1
elensiel <elensiel+github@proton.me>
2025-09-12 14:04:05 +0800

    forgot to update the antiX one

--- antiX/spotdl-macro.sh
+++ antiX/spotdl-macro.sh
@@ -1,32 +1,26 @@
-# this macro has different directories
-# due to poor decision on partitioning
+# diff dir for this one due to
+# poor decision on partitioning
 # and tinatamad pako mag reinstall
 
-source ../constants.sh
-
-CACHE_DIR="$HOME_DIR/.spotdl/.spotipy"
-OUTPUT_DIR="/srv/shared-files/music/spotdl"
-OUTPUT_FORMAT="{artists} - {title}.{output-ext}"
-SAVE_FILE="$HOME_DIR/.scripts/config/save.spotdl"
-PLAYLIST_LINK="https://open.spotify.com/playlist/5gz9xQFbxnDqMpRTzKjKUI?si=e79d9c3f704e4017"
-
+source ./constants.sh
 
 # just for version checking
 echo -e "\e[32mCurrent version\e[0m"
-# spotdl --version
+spotdl --version
 
 # check updates only
 # since some versions are broken
 echo -e "\e[32mChecking for updates\e[0m"
-# spotdl --check-for-updates
+spotdl --check-for-updates
 
 # ensure directories bfr proceeding
 mkdir -p "$OUTPUT_DIR"
+# overwrite config since spotdl doesn't have a command for it 
 cp -f "$HOME_DIR/.scripts/config/spotdl-config.json" "$HOME_DIR/.spotdl/config.json"
 
 spotdl sync \
-    "$PLAYLIST_LINK" \
-    --cache-path "$CACHE_DIR" \
+    "https://open.spotify.com/playlist/5gz9xQFbxnDqMpRTzKjKUI?si=e79d9c3f704e4017" \
+    --cache-path "$HOME_DIR/.spotdl/.spotipy" \
     --config \
-    --output "$OUTPUT_DIR/$OUTPUT_FORMAT" \
-    --save-file "$SAVE_FILE" \
+    --output "/srv/shared-files/music/spotdl/{artists} - {title}.{output-ext}" \
+    --save-file "$HOME_DIR/.scripts/config/save.spotdl" \
