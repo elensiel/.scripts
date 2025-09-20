f5b6222df159a232def8964c48660064abeef7c7
elensiel <elensiel+github@proton.me>
2025-09-20 03:38:22 -0400

    ensure dir on clamscan

--- clamscan-downloads.sh
+++ clamscan-downloads.sh
@@ -1,3 +1,6 @@
 source /home/$USER/.scripts/constants.sh
 
-sudo clamscan -r "$HOME_DIR/Downloads" --quiet --move="$HOME_DIR/.local/share/Trash/files" --log="$LOG_DIR/clamscan.log"
+QUARANTINE_DIR="$HOME_DIR/.local/share/Trash/files"
+mkdir -p QUARANTINE_DIR
+
+sudo clamscan -r "$HOME_DIR/Downloads" --quiet --move="$QUARANTINE_DIR" --log="$LOG_DIR/clamscan.log"
