937c24d5afc9e32e41b5b14b30f66041993143fd
elensiel <elensiel+github@proton.me>
2025-09-26 11:35:36 +0800

    fixed junk not being cleaned properly

--- clean-junk.sh
+++ clean-junk.sh
@@ -8,7 +8,7 @@ echo "===== $(get_timestamp) : START of Cleaning. =====" | sudo tee -a "$LOG_FIL
 # del unused dir first which are most likely removed programs
 echo '' | sudo tee -a "$LOG_FILE" 2>&1
 echo "-----> Removed empty cache directories... <-----" | sudo tee -a "$LOG_FILE" 2>&1
-find "$HOME_DIR/.cache" -type d -empty -print -delete
+find "$HOME_DIR/.cache" -mindepth 1 -type d -empty -print -delete
 
 # then actually del cache here
 echo '' | sudo tee -a "$LOG_FILE" 2>&1
@@ -19,6 +19,6 @@ find "$HOME_DIR/.cache" -type f -print -delete
 echo '' | sudo tee -a "$LOG_FILE" 2>&1
 echo "-----> Deleted from Trash... <-----" | sudo tee -a "$LOG_FILE" 2>&1
 ls -a "$HOME_DIR/.local/share/Trash/files" | sudo tee -a "$LOG_FILE" 2>&1
-rm -rf "$HOME_DIR/.local/share/Trash/files/*"
+rm -rf "$HOME_DIR/.local/share/Trash/files/"*
 
 echo "===== $(get_timestamp) : END of Cleaning. =====" | sudo tee -a "$LOG_FILE" 2>&1
