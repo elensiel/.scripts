17aabd9dbf12639e9a8c9ce37bf839ca0b08f4e3
elensiel <elensiel+github@proton.me>
2026-04-02 20:34:39 +0800

    migrated deletion from using rm -> find

--- macro/clean-junk.sh
+++ macro/clean-junk.sh
@@ -16,16 +16,14 @@ echo -e "===== $(get_current_date_and_time) : START of Cleaning. =====\n"
 # we do not delete unempty directories as they can break
 # its respected program
 echo "-----> Removed empty cache directories... <-----"
-find "$CACHE_DIR" -mindepth 1 -type d -empty \
-    -print -delete
+find "$CACHE_DIR" -mindepth 1 -type d -empty -print -delete
 
 # then actually del cache here
 echo -e "\n-----> Deleted cache... <-----"
-find "$CACHE_DIR" -mindepth 1 -type f \
-    -print -delete
+find "$CACHE_DIR" -mindepth 1 -type f -print -delete
 
 # trash is trash
 echo -e "\n-----> Deleted from Trash... <-----"
-rm -rfv "$HOME_DIR/.local/share/Trash/files/"*
+find "$HOME_DIR/.local/share/Trash/files/" -mindepth 1 -print -delete
 
 echo -e "\n===== $(get_current_date_and_time) : END of Cleaning. =====\n"
