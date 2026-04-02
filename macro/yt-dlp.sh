63c345a3f03b383a9a719837f6bf23fb83147e1d
elensiel <elensiel+github@proton.me>
2026-04-02 20:31:49 +0800

    removed hardcoded url and made it into an argument instead

--- macro/yt-dlp.sh
+++ macro/yt-dlp.sh
@@ -4,4 +4,4 @@ source "/home/$USER/.scripts/lib/path.sh"
 
 yt-dlp \
     --config-locations "$CONFIG_DIR/yt-dlp.conf" \
-    'https://youtube.com/playlist?list=PLTrEfzLrz97-WJ2SYXbCg-SVzq0F9YgHM&si=axtKvSeZFodjATC4'
+    "$@"
